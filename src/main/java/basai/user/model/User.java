e53436d0fc0c9f4eb1ba60e44de52191468b05b0
ShulavShakya <shakyashulav29@gmail.com>
2026-05-05 20:37:44 +0545

    Modified the User model for early use and removed .gitkeep files

--- src/main/java/basai/user/model/User.java
+++ src/main/java/basai/user/model/User.java
@@ -156,12 +156,11 @@ public class User {
         this.role = role;
     }
 
-    public User(int user_id, String name, String email, String password, VerificationStatus verificationStatus, String role) {
+    public User(int user_id, String name, String email, String password, String role) {
         this.user_id = user_id;
         this.name = name;
         this.email = email;
         this.password = password;
-        this.verificationStatus = verificationStatus;
         this.role = role;
     }
 }
